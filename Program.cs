21bf37b195855aa90e28d51c9cc31a6d4ff89c28
Bryan José <bryanfreitasmg@gmail.com>
2025-07-17 20:38:58 -0300

    Aprendendo C#

--- Program.cs
+++ Program.cs
@@ -4,53 +4,75 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("==========CALCULADORA SIMPLES==========");
-        Console.WriteLine("Digite o primeiro numero:");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        bool varContinue = true;
 
-        Console.WriteLine("Digite o segundo numero:");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Escolha a operação:");
-        Console.WriteLine("1- Adição (+)");
-        Console.WriteLine("2- Subtração (-)");
-        Console.WriteLine("3- Multiplicação (*)");
-        Console.WriteLine("4- Divisão (/)");
-        Console.WriteLine("Digite o numero da operação");
-        int operation = Convert.ToInt32(Console.ReadLine());
-
-        double result = 0;
-
-        //Condiçoes 
-
-        if (operation == 1)
+        while (varContinue)
         {
-            result = num1 + num2;
-        }
-        else if (operation == 2)
-        {
-            result = num1 - num2;
-        }
-        else if (operation == 3)
-        {
-            result = num1 * num2;
-        }
-        else if (operation == 4)
-        {
-            if (num2 != 0)
+            Console.WriteLine("==========CALCULADORA SIMPLES==========");
+            Console.WriteLine("Digite o primeiro numero:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1- Adição (+)");
+            Console.WriteLine("2- Subtração (-)");
+            Console.WriteLine("3- Multiplicação (*)");
+            Console.WriteLine("4- Divisão (/)");
+            Console.WriteLine("Digite o numero da operação");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            //Condiçoes 
+
+            if (operation == 1)
             {
-                result = num1 / num2;
+                result = num1 + num2;
+            }
+            else if (operation == 2)
+            {
+                result = num1 - num2;
+            }
+            else if (operation == 3)
+            {
+                result = num1 * num2;
+            }
+            else if (operation == 4)
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisao por zero nao é permitida.");
+                }
             }
             else
             {
-                Console.WriteLine("Erro: Divisao por zero nao é permitida.");
+                Console.WriteLine("Operação inválida.");
             }
-        }
-        else
-        {
-            Console.WriteLine("Operação inválida.");
+
+            Console.WriteLine("\nResultado = " + result + "\n");
+
+            Console.Write("Deseja continuar? (s)/(n)");
+            String response = Console.ReadLine();
+
+            if (response != "s")
+            {
+                varContinue = false;
+
+                Console.WriteLine("\nPrograma encerrado");
+            }
+            else
+            {
+                Console.WriteLine("\nContinuando...");
+            }
+
+            
         }
 
-        Console.WriteLine("\nResultado = " + result);
     }
 }
--- obj/Debug/net8.0/Aula10.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aula10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4bde15ab1f54ad3f3757a8282a6fa6b9e22aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
